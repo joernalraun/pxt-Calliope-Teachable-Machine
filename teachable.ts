e04b5f0537de52a791beebd5a0a89954dd7aa3e6
Jørn Alraun <joern.alraun@calliope.cc>
2025-11-21 18:39:52 +0100

    Update enum and function annotations in teachable.ts

--- teachable.ts
+++ teachable.ts
@@ -3,27 +3,27 @@
 // Aktuell statisch: Klasse 1–3
 
 enum ClassName {
-    //% block="Klasse 1"
-    Klasse1 = 1,
-    //% block="Klasse 2"
-    Klasse2 = 2,
-    //% block="Klasse 3"
-    Klasse3 = 3
+//% block="Klasse 1"
+Klasse1 = 1,
+//% block="Klasse 2"
+Klasse2 = 2,
+//% block="Klasse 3"
+Klasse3 = 3
 }
 
 //% blockNamespace=teachable
-//% color=#9ab5bd
+//% color=#BBEF53
 namespace teachable {
 
-    /**
-     * Runder Wertblock nur mit Dropdown.
-     * Der Block zeigt *nur* die Klassen-Auswahlliste an.
-     */
-    //% block="%klasse"
-    //% blockId=teachable_class_value
-    //% klasse.defl=ClassName.Klasse1
-    //% blockHidden=false
-    export function klasse(klasse: ClassName): ClassName {
-        return klasse;
-    }
+/**
+* Runder Wertblock nur mit Dropdown.
+* Der Block zeigt *nur* die Klassen-Auswahlliste an.
+*/
+//% block="Klasse: %klasse"
+//% blockId=teachable_class_value
+//% klasse.defl=ClassName.Klasse1
+//% blockHidden=false
+export function klasse(klasse: ClassName): ClassName {
+return klasse;
+}
 }
